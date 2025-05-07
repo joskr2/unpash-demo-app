1dc577c67ecd94b7223dd80c82d38aad4d619a8b
joskr2 <josretamozovar@gmail.com>
2025-05-07 11:10:28 -0500

    Add Pexels API integration and search component
    - Implemented Pexels API router for searching photos.
    - Added PexelsSearch component for user interaction and displaying results.
    - Updated environment variables to include Pexels API URL and key.
    - Refactored Home component to include PexelsSearch.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
+import { pexelsRouter } from "~/server/api/routers/pexels"; 
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +8,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-	post: postRouter,
+  post: postRouter,
+  pexels: pexelsRouter,
 });
 
 // export type definition of API
