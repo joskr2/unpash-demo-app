78238b0a1468576539bb90572166e0c49df0d5f8
joskr2 <josretamozovar@gmail.com>
2025-05-07 12:55:10 -0500

    Refactor env.js for improved code formatting and consistency

--- src/env.js
+++ src/env.js
@@ -2,19 +2,19 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-	server: {
-		NODE_ENV: z.enum(["development", "test", "production"]),
-		PEXELS_API_URL: z.string().url(),
-		PEXELS_API_KEY: z.string().min(1),
-	},
-	client: {
-		// NEXT_PUBLIC_CLIENTVAR: z.string(),
-	},
-	runtimeEnv: {
-		NODE_ENV: process.env.NODE_ENV,
-		PEXELS_API_URL: process.env.PEXELS_API_URL,
-		PEXELS_API_KEY: process.env.PEXELS_API_KEY,
-	},
-	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-	emptyStringAsUndefined: true,
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
+    PEXELS_API_URL: z.string().url(), 
+    PEXELS_API_KEY: z.string().min(1),
+  },
+  client: {
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    PEXELS_API_URL: process.env.PEXELS_API_URL,
+    PEXELS_API_KEY: process.env.PEXELS_API_KEY,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
 });
